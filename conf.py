40b51d2b8910a59d4fce51f0aa33a1249441f164
Adrien de Forceville <adriendeforceville@MacBook-Pro-de-Adrien.local>
2021-06-03 17:01:53 +0200

    Your Message

--- conf.py
+++ conf.py
@@ -30,7 +30,8 @@ author = 'Adrien de Forceville'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.autodoc' , 'sphinx_autodoc_typehints'] #,'sphinx_gallery.gen_gallery']
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'] #, 'sphinx_autodoc_typehints'] #,'sphinx_gallery.gen_gallery']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
