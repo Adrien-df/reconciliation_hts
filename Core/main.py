e226d2b3c6c60a62115d96d679de12dab58c7172
Adrien de Forceville <adeforceville@gmail.com>
2021-08-02 19:50:42 +0200

    adding mintSH method + improving plotting:wq

--- Core/main.py
+++ Core/main.py
@@ -7,7 +7,6 @@ import sklearn.metrics
 
 if __name__ == "__main__":
     #input = input()
-    print('\n Here is the output \n')
 
     data_m5 = pd.read_pickle("Data_Examples/M5_preprocessed.pkl")
     error_matrix = np.load("Data_Examples/error_matrix_500_to_600.npy",allow_pickle=True)
@@ -19,9 +18,9 @@ if __name__ == "__main__":
    
     object = To_Reconcile(data = data_m5, base_forecasts= predictions,columns_labels_ordered=['state_id','store_id', 'cat_id', 'dept_id'],in_sample_error_matrix=error_matrix,real_values=reals)
     object.compute_summing_mat()
-    #print(object.reconcile(method='MinTSa'))
-    #print(object.cross_val_score(metrics='rmse',reconcile_method='MinTSa',test_all=True))
-    object.plot(level='state_id',reconcile_method='VS')
+    #object.reconcile(method='MinTSh')
+    print(object.cross_val_score(metrics='mase',reconcile_method='MinTSh',test_all=True))
+    object.plot(level='total',reconcile_method='MinTSh')
 
     #print(data_m5['prediction'])
     
--- Core/reconciliation.py
+++ Core/reconciliation.py
@@ -11,6 +11,8 @@ from sklearn.metrics import mean_absolute_error
 import random
 import utils
 import matplotlib.pyplot as plt
+from statsmodels.stats.moment_helpers import cov2corr
+
 
 
 
@@ -28,6 +30,7 @@ class To_Reconcile:
         columns_labels_ordered: Optional[list[str]] = None,
         real_values: Optional[ArrayLike] = None,
         #reconciled_forecasts : ArrayLike = None
+        lambd = None
     ) -> None:
         self.data = data
         self.summing_mat = summing_mat
@@ -36,6 +39,7 @@ class To_Reconcile:
         self.columns_labels_ordered = columns_labels_ordered
         #self.reconciled_forecasts = reconciled_forecasts
         self.real_values = real_values
+        self.lambd = lambd
 
     def _check_parameters(self)->None :
         """[summary]
@@ -127,11 +131,27 @@ class To_Reconcile:
             i+=1
         return (n-i)
 
+    def _compute_lambda(
+        self
+    )->float :
+        res=np.matrix(self.in_sample_error_matrix).T
+        number_error_vectors = res.shape[0]
+        covm = utils.cross_product(res)/number_error_vectors
+        corm = cov2corr(covm)
+        xs = utils.scale(res,np.sqrt(np.diag(covm)))
+        v= (1/(number_error_vectors * (number_error_vectors - 1))) * (utils.cross_product(np.square(np.matrix(xs))) - 1/number_error_vectors * (np.square(utils.cross_product(np.matrix(xs))) ))
+        np.fill_diagonal(v,0)
+        corapn = cov2corr(np.diag(np.diag(covm)))
+        d = np.square((corm - corapn))
+        lambd = v.sum()/d.sum()
+        lambd= max(min(lambd,1), 0)
+        self.lambd = lambd
+        return(lambd)
 
     def reconcile(     
         self,
         method: Optional[str] = 'MintSa',
-        index_to_reconcile: Optional[int] = -1,
+        column_to_reconcile: Optional[int] = -1,
         reconcile_all: Optional[bool] =False
     ) -> ArrayLike :
 
@@ -154,20 +174,27 @@ class To_Reconcile:
         elif method in ['VS','MinTSa','MinTSh']:
 
             number_error_vectors = self.in_sample_error_matrix.shape[1]
-            #print(number_error_vectors)
             W1=np.zeros((self.summing_mat.shape[0],self.summing_mat.shape[0]))
             #return(self.in_sample_error_matrix[:,1][:,None])@(self.in_sample_error_matrix[:,1][:,None].T)
             for i in range(number_error_vectors):
                 W1+=(self.in_sample_error_matrix[:,i][:,None])@(self.in_sample_error_matrix[:,i][:,None].T)
             
             W1=W1/number_error_vectors
+            #print(W1)
 
             if method == 'VS' :                
                 W=np.diag(np.diag(W1))
             elif method == 'MinTSa' :
                 W=W1
             elif method == 'MinTSh' :
-                W= W1 #TODO MINTSHRINK
+                if self.lambd ==None :
+                    
+                    lambd = self._compute_lambda()
+                    print("lambda parameter is equal to ",lambd)
+                else : 
+                    lambd = self.lambd
+                
+                W=lambd*np.diag(np.diag(W1)) + (1-lambd)*W1
 
             combination_matrix = np.linalg.inv((self.summing_mat.T)@np.linalg.inv(W)@self.summing_mat)@(self.summing_mat.T)@np.linalg.inv(W)
 
@@ -181,10 +208,10 @@ class To_Reconcile:
 
         if reconcile_all :
             return( [self.summing_mat@combination_matrix@self.base_forecasts[:,i] for i in len(self.base_forecasts)])
-        elif index_to_reconcile==-1 :
+        elif column_to_reconcile==-1 :
             return(self.summing_mat@combination_matrix@self.base_forecasts)
         else :
-            return(self.summing_mat@combination_matrix@self.base_forecasts[:,index_to_reconcile])
+            return(self.summing_mat@combination_matrix@self.base_forecasts[:,column_to_reconcile])
 
 
 
@@ -238,7 +265,7 @@ class To_Reconcile:
             mean_score_reconciled = 0
             for index in indexes :
                 mean_score_real +=mean_squared_error(self.real_values[:,index],self.base_forecasts[:,index],squared=False)
-                mean_score_reconciled+=mean_squared_error(self.real_values[:,index],self.reconcile(method=reconcile_method,index_to_reconcile=index),squared=False)
+                mean_score_reconciled+=mean_squared_error(self.real_values[:,index],self.reconcile(method=reconcile_method,column_to_reconcile=index),squared=False)
 
         elif metrics=='mase' :
 
@@ -246,7 +273,7 @@ class To_Reconcile:
             mean_score_reconciled = 0
             for index in indexes :
                 mean_score_real +=mean_absolute_error(self.real_values[:,index],self.base_forecasts[:,index])
-                mean_score_reconciled+=mean_absolute_error(self.real_values[:,index],self.reconcile(method=reconcile_method,index_to_reconcile=index))
+                mean_score_reconciled+=mean_absolute_error(self.real_values[:,index],self.reconcile(method=reconcile_method,column_to_reconcile=index))
 
         elif metrics=='mse' :
 
@@ -254,7 +281,7 @@ class To_Reconcile:
             mean_score_reconciled = 0
             for index in indexes :
                 mean_score_real +=mean_squared_error(self.real_values[:,index],self.base_forecasts[:,index])
-                mean_score_reconciled+=mean_squared_error(self.real_values[:,index],self.reconcile(method=reconcile_method,index_to_reconcile=index))
+                mean_score_reconciled+=mean_squared_error(self.real_values[:,index],self.reconcile(method=reconcile_method,column_to_reconcile=index))
     
         else :
             raise ValueError(
@@ -271,22 +298,31 @@ class To_Reconcile:
         self,
         level: Optional[str] ='total',
         reconcile_method: Optional[str] = 'MinTSa', 
-        indexes: Optional[ArrayLike]= -1  ,
-        plot_real: Optional[bool] = True 
+        columns: Optional[ArrayLike]= -1  ,
+        plot_real: Optional[bool] = True,
                       
     ) -> None :
     #asser that we hace the real values 
     #assert that plot in total +columns label ordered
         indexes_of_series = self._get_indexes_level()
 
-        if indexes==-1 :
-            indexes=np.arange(self.real_values.shape[1])
-        for index in indexes_of_series[level] :
-            plt.plot(self.base_forecasts[index,indexes],color='blue')
-            plt.plot(np.asarray([self.reconcile(method=reconcile_method,index_to_reconcile=i) for i in indexes]).T[index],color='green')
+        if columns==-1 :
+            columns=np.arange(self.real_values.shape[1])
+        plt.figure(figsize=(15,6))
+        if level=='total':
+            plt.plot(self.base_forecasts[0,columns],color='red')
+            plt.plot(np.asarray([self.reconcile(method=reconcile_method,column_to_reconcile=i) for i in columns]).T[0],color='green')
             if plot_real :
-                plt.plot(self.real_values[index,indexes],color='red')
-            plt.title(f"Je dois trouver un titre")
+                plt.plot(self.real_values[0,columns],color='blue')
+
+        elif level!='total':
+            for index in indexes_of_series[level] :
+                plt.plot(self.base_forecasts[index,columns],color='red')
+                plt.plot(np.asarray([self.reconcile(method=reconcile_method,column_to_reconcile=i) for i in columns]).T[index],color='green')
+                if plot_real :
+                    plt.plot(self.real_values[index,columns],color='blue')
+        if plot_real :       
+            plt.title(f"Real (blue), predicted (red), and reconciled with {reconcile_method} method (green) values for the {level} aggregation level")
         plt.show()
 
         
