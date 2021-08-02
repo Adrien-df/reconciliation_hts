5f62708686c5daa5ebba939e8fbd5a30dfd2d96b
Adrien de Forceville <adeforceville@gmail.com>
2021-08-02 20:39:06 +0200

    new files

---
+++ Core/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def cross_product(matrix):
+    n=matrix.shape[0]
+    output = np.zeros([matrix.shape[1],matrix.shape[1]])
+    for i in range(n):
+        mat = matrix[i].T@matrix[i]
+        output+=mat
+    return(output)
+
+def scale(matrix,scales):
+    p,n = (matrix.shape[1],matrix.shape[0])
+    scaled = np.zeros([n,p])    
+    for j in range(p):
+        for i in range(n):       
+            scaled[i,j]=matrix[i,j]/scales[j]
+    return(scaled)
